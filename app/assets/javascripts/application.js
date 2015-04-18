c7d47d1451f504ee9baa31e7f0a84b2c6d34ac08
xcf1992 <xcf1992@gmail.com>
2015-04-17 17:21:38 -0700

    change style of confirm popup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,3 +49,39 @@ function showException(value){
             $('#except_time').hide();
     }
 }
+
+//GLOBAL JQuery Functaionality
+$(function(){
+ 
+    /*** CONFIRM MODAL OVERRIDE ***/
+    //override the use of js alert on confirms
+    //requires bootstrap3-dialog from https://github.com/nakupanda/bootstrap3-dialog
+    $.rails.allowAction = function(link){
+        if( !link.is('[data-confirm]') )
+            return true;
+        BootstrapDialog.show({
+            type: BootstrapDialog.TYPE_DANGER,
+            title: 'Confirm',
+            message: link.attr('data-confirm'),
+            buttons: [{
+                label: 'Accept',
+                cssClass: 'btn-primary',
+                action: function(dialogRef){
+                    link.removeAttr('data-confirm');
+                    link.trigger('click.rails');
+                    dialogRef.close();
+                }
+            }, {
+                label: 'Cancel',
+                action: function(dialogRef){
+                    dialogRef.close();
+                }
+            }]
+        });
+        return false; // always stops the action since code runs asynchronously
+    };
+ 
+  //Other global javascript stuffs
+  //...
+ 
+});
