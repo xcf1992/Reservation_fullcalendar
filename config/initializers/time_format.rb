e8750c0d8595626b3b1a19ea86d1f0b4c8db7873
xcf1992 <xcf1992@gmail.com>
2015-05-01 14:15:29 -0700

    change email date format

--- config/initializers/time_format.rb
+++ config/initializers/time_format.rb
@@ -1,3 +1,4 @@
 Time::DATE_FORMATS[:short_time] = "%I:%M %p"
 Time::DATE_FORMATS[:long_time] = "%Y - %m - %d"
 Time::DATE_FORMATS[:full_time] = "%Y - %m - %d %I:%M %p"
+Time::DATE_FORMATS[:read_time] = "%B %d, %Y at %I:%M %p"
