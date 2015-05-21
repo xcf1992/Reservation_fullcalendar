5165b159c416c545a6aceb06ced3ee8445ff3250
xcf1992 <xcf1992@gmail.com>
2015-05-21 16:53:43 -0700

    refine other tables in the web

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,7 +6,7 @@ class TestsController < ApplicationController
   # GET /tests
   # GET /tests.json
   def index
-    @tests = Test.search(params[:search]).filter(params[:ctPositive], params[:ngPositive], params[:result]).order(sort_column + " " + sort_direction).page(params[:page]).per(15)
+    @tests = Test.search(params[:search]).filter(params[:ctPositive], params[:ngPositive], params[:result]).order(sort_column + " " + sort_direction).page(params[:page]).per(3)
     @download = Test.search(params[:search]).filter(params[:ctPositive], params[:ngPositive], params[:result]).order(sort_column + " " + sort_direction)
     puts @download.size
     puts @tests.size
